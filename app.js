6517f284b254038e925ba27725d5ae61946ed664
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-04-04 15:55:53 -0400

    randomize card images, fixed css to hide image

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const gameBoard = document.querySelector("#game");
-let cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
+let flippedCards = document.querySelectorAll(".flipped");
 const startOver = document.querySelector("#startOver");
+const cardList = ["&#128049", "&#128049", "&#128054", "&#128054", "&#129418", "&#129418", "&#129437", "&#129437", "&#128052", "&#128052", "&#128045", "&#128045", "&#128048", "&#128048", "&#128035", "&#128035", "&#129417", "&#129417", "&#128013", "&#128013", "&#128030", "&#128030", "&#128032", "&#128032"];
 
-for (let check of cards){
-    if (check.classList.contains("down")){
-        check.innerText= "?";
+function shuffleCards (arr){
+    let value;
+    for (let i=arr.length-1; i >0; i--){
+        const j = Math.floor(Math.random()*i+1);
+       value = arr[i];
+       arr[i]=arr[j];
+       arr[j]=value;
     }
-    if (check.classList.contains("flipped")){
-        check.innerText= '';
+    return arr;
+}
+
+
+function setCardValues (){
+    for (let i=0; i < flippedCards.length; i++){
+        flippedCards[i].innerHTML=cardList[i];
     }
 }
 
-gameBoard.addEventListener("click", function(e){
-    e.target.classList.remove("down");
-    e.target.classList.add("flipped");
-})
+shuffleCards(cardList);
+
+setCardValues();
+
+
 
 startOver.addEventListener("click", function(e){ 
     e.preventDefault();
-    for (card of cards){
-        if (card.classList.contains("flipped")){
-            card.classList.remove("flipped");
-            card.classList.add("down");
-        }
-    }
-})
+    shuffleCards(cardList);
+    setCardValues();
+    })
+
