89df27348e352c21a6ee83201b9ae35c938a7428
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-04-05 16:45:39 -0400

    added game logic, local storage best score, prevent user from clicking third card or score going up if section of gameboard that isn't a card is clicked

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const cards = document.querySelectorAll(".card");
 let flippedCards = document.querySelectorAll(".flipped");
 const startOver = document.querySelector("#startOver");
 const cardList = ["&#128049", "&#128049", "&#128054", "&#128054", "&#129418", "&#129418", "&#129437", "&#129437", "&#128052", "&#128052", "&#128045", "&#128045", "&#128048", "&#128048", "&#128035", "&#128035", "&#129417", "&#129417", "&#128013", "&#128013", "&#128030", "&#128030", "&#128032", "&#128032"];
+const currentScore = document.querySelector("#currentScore");
+const bestScore = document.querySelector("#currentBest");
+let score = 0;
+let cardPair = [];
+let cardPairSource = [];
+let cardPairFound = [];
+
+
 
 function shuffleCards (arr){
     let value;
@@ -22,15 +30,85 @@ function setCardValues (){
     }
 }
 
+function setBestScore(){
+if (localStorage.getItem("lowestScore") !== null){
+    bestScore.innerText = parseFloat(localStorage.getItem("lowestScore"));
+}
+}
+
+setBestScore();
+
 shuffleCards(cardList);
 
 setCardValues();
 
+gameBoard.addEventListener("click", function (e){
+    console.log(e.target);
+})
+
+gameBoard.addEventListener("click", function (e){
+    if (e.target.classList.contains("flipped") || !e.target.classList.contains("card") || cardPair.length === 2){
+        return null;
+    } else if (e.target.classList.contains("down")){
+        e.target.parentElement.classList.add("flip");
+        cardPair.push(e.target.nextElementSibling.innerHTML);
+        cardPairSource.push(e.target.parentElement);
+    }
+    else {
+        e.target.classList.add("flip");
+        cardPair.push(e.target.lastElementChild.innerHTML);
+        cardPairSource.push(e.target);
+    }
+})
+
+gameBoard.addEventListener("click", function(){
+    if (cardPair.length === 2){
+        if (cardPair[0] !== cardPair[1]){
+            setTimeout(function (){
+                for (let card of cardPairSource){
+                    card.classList.remove("flip");
+                    cardPair = [];
+                    cardPairSource = [];
+                }
+            }, 1000);
+        } else {
+            cardPairFound.push([cardPair[0], cardPair[1]])
+            cardPair = [];
+            cardPairSource = [];
+            if (cardPairFound.length === (cardList.length)/2){
+                if (currentScore.innerText +1 < bestScore.innerText || bestScore.innerText === ''){
+                    bestScore.innerText = parseFloat(currentScore.innerText)+1;
+                    localStorage.removeItem("lowestScore");
+                    localStorage.setItem("lowestScore", bestScore.innerText);
+                }
+                setTimeout(function(){
+                    alert("You win!");}, 750);
+            }
+        }
+    }
+})
+
+gameBoard.addEventListener("click", function(e){
+    if (e.target.classList.contains("flipped") || !e.target.classList.contains("card")){
+        score += 0;
+    } else {
+        score += 1;
+    }
+    currentScore.innerText = score;
+})
 
 
 startOver.addEventListener("click", function(e){ 
     e.preventDefault();
+    for (let card of cards){
+        if (card.classList.contains("flip")){
+            card.classList.remove("flip");
+        }
+    }
+    cardPairFound = [];
     shuffleCards(cardList);
     setCardValues();
+    score=0;
+    currentScore.innerText=score;
     })
 
